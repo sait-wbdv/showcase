1e372bc38703882651f4d1fe79ae8eded8c8ae99
Patrick Bello <mayorbello@yahoo.com>
2020-12-14 18:44:18 -0700

    updating skills, taglines and adding .gitignore

--- js/client.js
+++ js/client.js
@@ -22,19 +22,19 @@ fetch('https://skulls-standings.herokuapp.com/api/v0/students/')
     <div class="card">
     <div class="topprofile">
       <h1 class="name">${student.name}</h1>
-      <img class="profpic" src="assets/images/${firstName}.jpg" alt="pic">
+      <img class="profpic" src=${(student.links[0].imageLink.length) ? student.links[0].imageLink  : (student.name === "Lauren Kruger") ? `assets/images/${firstName}.jpg` : 'assets/images/this_is_fine.jpg'}   alt="pic">
 
     </div>
     <div class="info">
-      <h3>card extraordinaire</h3>
+      <h3>${(student.tagline.length) ? student.tagline : 'insert tagline'}</h3>
     </div>
     <div class="links">
  <p class="linking"><a href=${student.links[0].username}>Github!</a></p>
- <p class="linking"><a href=${student.links[3].url}>Portfolio!</a></p>
+ <p class="linking"><a href=${student.links[2].link}>Portfolio!</a></p>
  </div>
       <hr>
     <div class="talents">
-      <p><strong>Skills: </strong> Master of cards, lover of colour</p>
+      <p><strong>Skills: </strong> ${student.skills.length ? student.skills.join(', ') : 'insert skills'}</p>
       <p><strong>Trophies:</strong> ${trophies.length ? `<span>${trophies.join(', ')}</span></p>` : `<i class="far fa-frown"></i>`}</p>
     </div>
   </div>
